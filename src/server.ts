2b00d7093efe3727fb4acfd99bd239470699c57e
avoliveira <adriano.vidal@castgroup.com.br>
2021-04-21 18:36:58 -0300

    Criado conexão com banco de dados. Criado primeira tabela e mapeamento do objeto. Rota para a tabela.

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,10 @@
-import express, { response } from "express";
+import express from "express";
+import "./database"
+import { routes } from "./routes";
 
 const app = express();
 
-/**
- * GET = Buscas
- * POST = Criação
- * PUT = Alteração
- * DELETE = Deletar
- * PATCH = Alterar uma informação específica
- */
-
-app.get("/", (request, response) => {
-  response.json({
-    message: "Olá NLV 05"
-  });
-})
-
-app.post("/users", (Request, response) => {
-  return response.json({message: "Usuário salvo com sucesso!"});
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
